a7028dad5a93c4bea35e8df96e336f2c0418a165
Sam <s@Sams-MacBook-Pro-2.local>
2018-12-17 08:37:17 +0900

    Updates

---
+++ precache-manifest.93a9516ff5b011b47d981ac4da83e81e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b474144f87fd6d7f800f",
+    "url": "/myapp/static/css/main.56a54097.chunk.css"
+  },
+  {
+    "revision": "b474144f87fd6d7f800f",
+    "url": "/myapp/static/js/main.b474144f.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/myapp/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "8dd91205e0d72f81ec3b",
+    "url": "/myapp/static/js/runtime~main.8dd91205.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myapp/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "382accf3064a12ce76083e9f31e5a535",
+    "url": "/myapp/index.html"
+  }
+];
